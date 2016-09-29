3c1a95797dbd733aa304c01d29ce4b14a915f1b0
st0495 <shashanktiwari158@gmail.com>
2016-09-29 10:11:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/article-two',function(req,res){
 });
 app.get('/article-three',function(req,res){
 
-    res.sendfile("Article three will be served here");
+    res.sendFile("Article three will be served here");
 });
 
 
